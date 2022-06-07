301d79c7dc1258b788a6ad2d5ae1d4a218d0b05e
tiegrun <tiko3020@gmail.com>
2022-06-07 02:32:46 -0400

    accesAllPages

--- src/Components/CreateQuestions/AddQuestions.js
+++ src/Components/CreateQuestions/AddQuestions.js
@@ -11,29 +11,6 @@ function AddQuestions({addQuestionAsync, loggedUser, isLogged}) {
   const [optionOne, setOptionOne] = useState("");
   const [optionTwo, setOptionTwo] = useState("");
   const [checkOptions, setCheckOptions] = useState(true);
-  const [isAllowed, setIsAllowed] = useState(true);
-
-  useEffect(()=>{
-
-    const userId = localStorage.getItem('userId');
-
-    if(isLogged){
-      
-      setIsAllowed(true)
-
-    }
-    else if(loggedUser!=="No User" || userId!==""){
-
-      setIsAllowed(true)
-
-    }
-    else{
-
-      setIsAllowed(false)
-
-    }
-
-  }, [isLogged])
 
   let navigate = useNavigate();
 
@@ -88,38 +65,31 @@ function AddQuestions({addQuestionAsync, loggedUser, isLogged}) {
   }
 
   return (
-    <Fragment>
-      { isAllowed 
-        ? 
-          <div className='Section LeaderBoard'>
-            <div className='pageHeader'>
-              <h2>Create A Question</h2>
-            </div>   
-            <div className='btnSection'> |
-              <button className='simpleBtn' onClick={toRoute} name={`/`}>Home</button> |
-              <button className='simpleBtn' onClick={toRoute} name={`/leaderboard`}>leaderboard</button> |
-            </div>
-            <SelectedUserProfilePage />
-            <div className='subSection'>
-              <div className="profileQuestionList">
-                <form>
-                  <label>Would you rather 
-                    <input type="text" value={optionOne} className='inputFields' minLength="1" maxLength = "160" placeholder='Option One' onChange={handleOptions} name="optionOne"/>
-                  </label>
-                  <label>or
-                    <input type="text" value={optionTwo} className='inputFields' minLength="1" maxLength = "160" placeholder='Option Two' onChange={handleOptions} name="optionTwo"/>
-                    ?
-                  </label>
-                  <button type="submit" className="simpleBtn addQuestionBtn" onClick={addQuestion} name={`/`}>Add This Question</button>
-                </form>
-              </div>
-              {!checkOptions ? <div className='subSection'>It Is Not Correct</div> : <Fragment></Fragment>}
-            </div>
-          </div> 
-        : 
-          <Error404Page />
-      }
-    </Fragment>
+    <div className='Section LeaderBoard'>
+      <div className='pageHeader'>
+        <h2>Create A Question</h2>
+      </div>   
+      <div className='btnSection'> |
+        <button className='simpleBtn' onClick={toRoute} name={`/`}>Home</button> |
+        <button className='simpleBtn' onClick={toRoute} name={`/leaderboard`}>leaderboard</button> |
+      </div>
+      <SelectedUserProfilePage />
+      <div className='subSection'>
+        <div className="profileQuestionList">
+          <form>
+            <label>Would you rather 
+              <input type="text" value={optionOne} className='inputFields' minLength="1" maxLength = "160" placeholder='Option One' onChange={handleOptions} name="optionOne"/>
+            </label>
+            <label>or
+              <input type="text" value={optionTwo} className='inputFields' minLength="1" maxLength = "160" placeholder='Option Two' onChange={handleOptions} name="optionTwo"/>
+              ?
+            </label>
+            <button type="submit" className="simpleBtn addQuestionBtn" onClick={addQuestion} name={`/`}>Add This Question</button>
+          </form>
+        </div>
+        {!checkOptions ? <div className='subSection'>It Is Not Correct</div> : <Fragment></Fragment>}
+      </div>
+    </div> 
   )
 }
 
--- src/Components/Questions/QuestionList.js
+++ src/Components/Questions/QuestionList.js
@@ -1,19 +1,17 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, {useState, useEffect} from 'react';
 import {mapStateToProps, mapDispatchToProps} from '../../Store/storeProps';
 import {connect} from "react-redux";
 import '../../Style/Questions.css'
 import UnAnsweredQuestions from './UnAnsweredQuestions';
 import AnsweredQuestions from './AnsweredQuestions';
 import {Routes, Route } from "react-router-dom";
-import Error404Page from "../Error404Page";
+// import Error404Page from "../Error404Page";
 
-function QuestionList({userAnswerQuestionType, isLogged, loggedUser}) {
+function QuestionList({userAnswerQuestionType}) {
 
   const [answerType, setAnswerType] = useState();
-  const [isAllowed, setIsAllowed] = useState(false);
 
   const userAnswerType = localStorage.getItem('answertype');
-  const userId = localStorage.getItem('userId');
 
   useEffect(()=>{
 
@@ -39,47 +37,15 @@ function QuestionList({userAnswerQuestionType, isLogged, loggedUser}) {
   }, [userAnswerQuestionType, userAnswerType]);
 
   
-  useEffect(()=>{
-
-    if(isLogged){
-      
-      setIsAllowed(true)
-
-    }
-    else if(loggedUser!=="No User" || userId!==""){
-
-      setIsAllowed(true)
-
-    }
-    else{
-
-      setIsAllowed(false)
-
-    }
-
-  }, [isLogged])
-  
   return (
-
-    <Fragment>
-      {isAllowed
-       ?
-        <Routes>
-          {!answerType
-            ? 
-              <Route path="/questions/:question_id"  element={<UnAnsweredQuestions/>}/>
-            :
-              <Route path="/questions/:question_id"  element={<AnsweredQuestions />}/>
-          }
-        </Routes> 
-      :
-        <Routes>
-          <Route path="/questions/:question_id"  element={<Error404Page />}/>
-        </Routes>
+    <Routes>
+      {!answerType
+        ? 
+          <Route path="/questions/:question_id"  element={<UnAnsweredQuestions/>}/>
+        :
+          <Route path="/questions/:question_id"  element={<AnsweredQuestions />}/>
       }
-    </Fragment>
-        
-           
+    </Routes>      
   )
 }
 
